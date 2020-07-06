63c49be126019bd81be77572961e8c2c17ceaaef
Administrator <yuy20170301@126.com>
2020-07-06 10:01:20 +0800

    ‘布局底部导航栏'

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,64 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import {
+ HashRouter as Router,
+  Switch,
+  Route,
+  Link,
+  Redirect,
+} from "react-router-dom";
+import styled from 'styled-components';
+import Nav from './components/Nav';
+const Wrapper=styled.div`
 
-function App() {
+height: 100vh;
+display: flex;
+flex-direction: column;
+`
+const Main=styled.main`
+
+flex-grow: 1;
+overflow: auto;
+`
+
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Wrapper>
+        <Main>
+          <Switch>
+            <Route path="/tags">
+              <Tags />
+            </Route>
+            <Route path="/money">
+              <Money />
+            </Route>
+            <Route path="/statistics">
+              <Statistics />
+            </Route>
+            <Redirect exact from='/' to="/money"/>
+            <Route path="*">
+              <NoMatch/>
+            </Route>
+          </Switch>
+        </Main>
+     <Nav/>
+
+      </Wrapper>
+
+    </Router>
   );
 }
+function NoMatch(){
+  return <div>你输入的地址不存在，请重新输入</div>
+}
+function Statistics() {
+  return <h2>statistics</h2>;
+}
 
-export default App;
+function Tags() {
+  return <h2>tag</h2>;
+}
+
+function Money() {
+  return <h2>money</h2>;
+}
