21278fe33bb0d2faaa157a13ed4ac0812839ab60
Administrator <yuy20170301@126.com>
2020-07-09 19:06:55 +0800

    '完成categorySection部分'

--- src/money/CategorySetion.tsx
+++ src/money/CategorySetion.tsx
@@ -1,9 +1,10 @@
 import styled from 'styled-components';
+import React, {useState} from 'react';
 
-const CategorySection=styled.section` 
+const Wrapper=styled.section` 
 
 font-size: 24px;
-  > ol{
+  > ul{
     display:flex;
     background-color: blue;
     > li {
@@ -23,4 +24,29 @@ font-size: 24px;
       }
     }
   }`
+const CategorySection=()=>{
+  const CategoryMap={
+    '-':'支出',
+    '+':'收入'
+  }
+  type X=typeof CategoryMap
+  type Y=keyof X
+  const [Category,setCategory]=useState('-')
+  const [CategoryList]=useState<Y[]>(['-','+'])
+  return(
+  <Wrapper>
+    <ul>
+      {CategoryList.map(t=>
+      <li key={t}
+        className={
+        Category===t?'selected':''
+      } onClick={()=>{
+        setCategory(t)
+      }}>{CategoryMap[t]}</li>
+    )}
+
+    </ul>
+  </Wrapper>
+  )
+}
 export {CategorySection}
--- src/views/Money.tsx
+++ src/views/Money.tsx
@@ -16,36 +16,15 @@ const MyLayout = styled(Layout)`
 function Money() {
   return (
     <MyLayout>
-      <CategorySection>
-        <ol>
-          <li className='selected'>支出</li>
-          <li>收入</li>
-        </ol>
 
-      </CategorySection>
+      <CategorySection/>
 
       <TagsSection/>
       <NoteSection/>
 
-      <NumberPadSection>
-        <div className="output">100</div>
-        <div className="pad">
-          <button>1</button>
-          <button>2</button>
-          <button>3</button>
-          <button>删除</button>
-          <button>4</button>
-          <button>5</button>
-          <button>6</button>
-          <button>清空</button>
-          <button>7</button>
-          <button>8</button>
-          <button>9</button>
-          <button className="ok">ok</button>
-          <button className="zero">0</button>
-          <button>.</button>
-        </div>
-      </NumberPadSection>
+
+
+      <NumberPadSection/>
     </MyLayout>);
 }
 
