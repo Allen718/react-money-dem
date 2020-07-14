864f02ae4cade36f31489260fa65d473f1f3459f
Administrator <yuy20170301@126.com>
2020-07-14 10:34:25 +0800

    '修改默认标签'

--- src/useTags.tsx
+++ src/useTags.tsx
@@ -1,15 +1,31 @@
-import {useState} from 'react';
+import {useEffect,  useState} from 'react';
 import createId from './lib/createId';
-
-const defaultTags = [{id: createId(), name: '外卖'}, {id: createId(), name: '蔬菜'}, {
-  id: createId(),
-  name: '零食'
-}, {id: createId(), name: '房租'}, {id: 5, name: '水果'}];
+import useUpdate from './hooks/useUpdate';
 const useTags = () => {
-  const [tags, setTags] = useState(defaultTags);
+  const [tags, setTags] = useState<{ id: number, name: string }[]>([]);
   const findTag = (id: number) => {
     return tags.filter(tag => tag.id === id)[0];
   };
+  const addTag = () => {
+    const tagName = window.prompt('请输入新增标签名');
+    if (tagName !== null) {
+      setTags([...tags, {id: createId(), name: tagName}]);
+    }
+  };
+  useEffect(() => {
+    let localTags = JSON.parse(window.localStorage.getItem('tags') || '[]');
+    if (localTags.length === 0) {
+    localTags= [{id: createId(), name: '外卖'}, {id: createId(), name: '蔬菜'}, {
+        id: createId(),
+        name: '零食'
+      }, {id: createId(), name: '房租'}, {id: 5, name: '水果'}];
+
+    }
+      setTags(localTags)
+
+    },[] )
+
+  useUpdate(() => {window.localStorage.setItem('tags', JSON.stringify(tags));}, [tags]);
   const findTagIndex = (id: number) => {
     let result = -1;
     for (let i = 0; i < tags.length; i++) {
@@ -21,9 +37,9 @@ const useTags = () => {
     return result;
   };
   const updateTag = (id: number, obj: { name: string }) => {
-   setTags(
-     tags.map((tag)=>tag.id===id?{id:id,name:obj.name}:tag)
-   )
+    setTags(
+      tags.map((tag) => tag.id === id ? {id: id, name: obj.name} : tag)
+    );
 
     // const index = findTagIndex(id);
     // const cloneTags = JSON.parse(JSON.stringify(tags));
@@ -32,12 +48,12 @@ const useTags = () => {
   };
 
   const deleteTag = (id: number) => {
-    setTags(tags.filter(tag=>tag.id!==id))
+    setTags(tags.filter(tag => tag.id !== id));
     // const index = findTagIndex(id);
     // const cloneTags = JSON.parse(JSON.stringify(tags));
     // cloneTags.splice(index, 1);
     // setTags(cloneTags);
   };
-  return {tags, setTags, findTag, updateTag,deleteTag,findTagIndex};
+  return {tags, setTags, findTag, updateTag, deleteTag, findTagIndex, addTag};
 };
 export default useTags;
